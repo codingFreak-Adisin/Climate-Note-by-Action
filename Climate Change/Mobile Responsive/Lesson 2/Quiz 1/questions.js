1083fef9067e6332f4491dcadf7a57c28bbc3528
Aditya Sinha <reena_mac1@yahoo.com>
2021-12-07 14:32:17 +0530

    New Version

---
+++ Climate Change/Mobile Responsive/Lesson 2/Quiz 1/questions.js
@@ -0,0 +1,50 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is global warming?",
+    answer: "The phenomenon of increasing average temperature near the surface of earth over the past two centuries",
+    options: [
+      "The phenomenon of decreasing average temperature near the surface of earth over the past two centuries",
+      "The phenomenon of increasing average temperature near the surface of earth over the past two centuries",
+      "Absorbs and emits radiant energy within the thermal infrared range",
+      "None",
+      "To Learn more about Global Warming, Visit -: https://climate.nasa.gov/resources/global-warming-vs-climate-change/ & https://bit.ly/306Ekeq"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which century climate scientist have gathered detailed observation of various weather phenomena?",
+    answer: "Mid-20th Century",
+    options: [
+      "19th Century",
+      "20th Century",
+      "Mid-20th Century",
+      "None",
+      "Learn more about History of Climate Change Science - https://en.wikipedia.org/wiki/History_of_climate_change_science"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What are the causes of global warming?",
+    answer: "Both A and B",
+    options: [
+      " Glaciers melting, sea-level rising",
+      "Cloud forests are dying and wildlife is scrambling to keep pace.",
+      "Both A and B",
+      "Maybe B",
+      "Learn more about Causes of Global Warming - https://bit.ly/3DBdr0L"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What is a greenhouse effect?",
+    answer: "The greenhouse effect is a process that occurs when gases in earth's atmosphere trap the sun heat.",
+    options: [
+      "The greenhouse effect is a process that occurs when gases are released out from earth's atmosphere.",
+      "The greenhouse effect is a process that occurs when gases in earth's atmosphere trap the sun heat.",
+      "Both A and B",
+      "None",
+      "Learn more about Green House Effect - https://go.nasa.gov/2YwrzJ5",
+    ]
+  },
+];
---
+++ Climate Change/Mobile Responsive/Lesson 7/Quiz 6/questions.js
@@ -0,0 +1,50 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What are some examples of sustainable infrastructure?",
+    answer: "Low-energy buildings",
+    options: [
+      "Low-energy buildings",
+      "High-energy buildings",
+      "skyscrapers",
+      "Both A and B",
+      "To learn more about the topic - https://bit.ly/3AKel9k"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What are 3R’s",
+    answer: "All of the above",
+    options: [
+      "The principle of reducing",
+      "Reusing",
+      "Recycling is often called the 3R’s",
+      "All of the above",
+      "To learn more about the topic - https://bit.ly/3j5I1Yo"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What causes ocean acidification? (General Question)",
+    answer: "CO2 dissolved in ocean water",
+    options: [
+      "CO2 dissolved in ocean water",
+      "Sunscreen in the water",
+      "Warm water",
+      "Sediment stirred up by extreme storms",
+      "To learn more about the topic - https://bit.ly/3j737Wb"
+    ]
+  },
+  {
+    numb: 4,
+    question: "How has the global average temperature changed since the Industrial Revolution? (General Question)",
+    answer: "Warmer by more than 1 degree C (2.1 degrees F)",
+    options: [
+      "Cooler by 0.1 degree C (0.2 degree F)",
+      "Warmer by 0.1 degree C (0.2 degree F)",
+      "Warmer by more than 1 degree C (2.1 degrees F)",
+      "Warmer by almost 2 degrees C (3.6 degrees F)",
+      "To learn more about the topic - https://go.nasa.gov/3BSsF0U"
+    ]
+  },
+];
